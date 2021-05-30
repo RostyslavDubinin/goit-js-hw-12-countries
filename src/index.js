bb79c87fa709a9b76775ad5daa36f63764fd25b2
RostyslavDubinin <75220575+RostyslavDubinin@users.noreply.github.com>
2021-05-30 22:01:05 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,60 @@
+
+import countryCardTpl from './templates/country-card.hbs';
+import allCountryCardTpl from './templates/allContries.hbs';
+import API from './js/fetchCountries.js';
+import getRefs from './js/get-refs.js';
+
+const refs = getRefs();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const searchQuery = form.elements.query.value;
+
+  
+
+
+
+
+
+// API.fetchCountries(searchQuery).then(GetData);
+// function GetData (qwe) {qwe.length;};
+
+
+if (searchQuery.length > 2){
+  API.fetchCountries(searchQuery)
+    .then(renderCountryCard)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+    
+}
+else {
+    API.fetchCountries(searchQuery)
+    .then(renderAllCountryCardTpl)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+}
+}
+
+function renderCountryCard(countries) {
+    const markup = countryCardTpl(countries);
+    refs.cardContainer.innerHTML = markup;
+    // console.log(countries);
+}
+
+function renderAllCountryCardTpl(countries) {
+    const markup = allCountryCardTpl(countries);
+    refs.cardContainer.innerHTML = markup;
+    
+}
+
+function onFetchError(error) {
+    alert('Too many matches found. Please enter a more specific query!');
+  }
+
+  
